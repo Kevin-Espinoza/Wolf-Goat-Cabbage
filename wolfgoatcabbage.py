84bd43ce3bf0733e5bc2b7cc3789379b69a09c7b
Kevin Espinoza <K.espinoza1012@gmail.com>
2022-09-28 02:35:32 -0700

    Completed
    Completed - All tests passed

--- wolfgoatcabbage.py
+++ wolfgoatcabbage.py
@@ -2,88 +2,65 @@
 from operator import truediv
 from unittest import result
 from search import *
-# YOUR CODE GOES HERE
 
 
 class WolfGoatCabbage(Problem):
     def __init__(self, initial=('F','G','W','C'), goal=()):
-        self.initial = frozenset(initial)
-        self.goal = frozenset(goal)
-        # super().__init__(self.initial, self.goal)
+        super().__init__(frozenset(initial), frozenset(goal))
 
     def goal_test(self, state):
-        # print('\nGOAL TEST - STATE: ', state)
-        # print('GOAL STATE: ', self.goal)
-        if state==set(self.goal):
-            return True
-        else:
-            return False
-
-    # This is a helper function that checks if the farmer is in the current state
-    def farmer(self, state):
-        if('F' in state):
-            return True
-        else:
-            return False
+        return True if state==set(self.goal) else False
 
     def result(self, state, action):
+        # Set the given state and action to sets for easy manipulation
         state = set(state)
         action = set(action)
-        # print('\nSTATE: ', state)
-        # print('ACTION: ', action)
 
-        test = self.farmer(state)
-        if(test and action.issubset(state)):
+        if('F' in state and action.issubset(state)):
             state = state-action
-            # state.remove('F')
-            # print('NEW STATE: ', state)
         elif(action.issubset(state)):
-            # state.add('F')
             state.remove(action)
-            # print('NEW STATE: ', state)
-        elif(test):
+        elif('F' in state):
             state.remove('F')
-            # print('NEW STATE: ', state)
         else:
-            state.union(action)
+            state=state.union(action)
             state.add('F')
-            # print('NEW STATE: ', state)
-            
+
+        # Must return a frozen set since hashable datatype is required
         return frozenset(state)
 
 
     def actions(self, state):
-        # print('ACTIONS')
-        # print('\n')
-        state_temp=set(state)
-        # print("INITIAL is:", self.initial)
-        print('STATE: ', state)
+        # Set the given state to a set for easy manipulation
+        state=set(state)
 
-        possible_actions = [{'F'}, {'G','F'}, {'W','F'}, {'C','F'}]
-        if state_temp == {'F', 'G', 'W', 'C'}:
-            possible_actions = [{'G','F'}]
-        elif state_temp == {'F', 'C', 'W'}:
-            possible_actions = [{'W','F'}]
-        elif state_temp == {'F', 'G', 'W'}:
-            possible_actions = [{'W','F'}]
-        elif state_temp == {'W', 'C'}:
-            possible_actions = [{'F'}, {'G','F'}]
-        elif state_temp == {'W', 'G'}:
-            possible_actions = [{'F'}, {'C','F'}]
-        elif state_temp == {'F', 'G'}:
-            possible_actions = [{'G','F'},]
-        elif state_temp == {'F', 'C'}:
-            possible_actions = [{'F'}, {'C','F'}] 
-        elif state_temp == {'F', 'W'}:
-            possible_actions = [{'F'}, {'W','F'}]
-        elif state_temp == {'W'}:
+        # Algo to solve the W-G-C problem 
+        possible_actions = [{'F'}, {'G','F'}, {'W','F'}, {'C','F'}] 
+        if state == {'F', 'G', 'W', 'C'}:
+            possible_actions = [{'F','G'}]
+        elif state == {'F', 'C', 'W'}:
+            possible_actions = [{'F', 'W'}, {'F','C'}]
+        elif state == {'F', 'G', 'W'}:
+            possible_actions = [{'F','W'}]
+        elif state == {'W', 'C'}:
             possible_actions = [{'F'}]
-        elif state_temp == {'G'}:
-            possible_actions = [{'F'}, {'W','F'}, {'C','F'}]
-        elif state_temp == {'C'}:
-            possible_actions = [{'G','F'}]
-        elif state_temp == {'F'}:
+        elif state == {'W', 'G'}:
             possible_actions = [{'F'}]
+        elif state == {'F', 'G'}:
+            possible_actions = [{'F', 'G'}]
+        elif state == {'F', 'C'}:
+            possible_actions = [{'F'}, {'F','C'}] 
+        elif state == {'F', 'W'}:
+            possible_actions = [{'F'}, {'F','W'}]
+        elif state == {'W'}:
+            possible_actions = [{'F', 'G'}]
+        elif state == {'G'}:
+            possible_actions = [{'F'}, {'F','W'}, {'F','C'}]
+        elif state == {'C'}:
+            possible_actions = [{'F','G'}]
+        elif state == {'F'}:
+            possible_actions = [{'F'}]
+            
         return possible_actions
 
 
